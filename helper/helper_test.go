3904d5961c942cfeabc7d41c2c34b079c9014ab1
Evan Harmon <evan.p.harmon@gmail.com>
2018-09-17 01:20:04 -0700

    feat(storage): implement table tests for helper

--- helper/helper_test.go
+++ helper/helper_test.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"testing"
 
-	test "github.com/evanharmon/eph-music-micro/helper/testhelper"
+	testhelper "github.com/evanharmon/eph-music-micro/helper/testhelper"
 )
 
 //
@@ -16,33 +16,46 @@ var (
 // setup sets the necessary vars from ENV exports
 func setup(t *testing.T) {
 	id, err := GetEnv(envVarProjectID)
-	test.Ok(t, err)
-	test.Assert(t, len(id) != 0, "Export necessary ENV vars before running tests")
+	testhelper.Ok(t, err)
+	testhelper.Assert(t, len(id) != 0, "Export necessary ENV vars before running tests")
 	projectID = id
 }
 
-// TestGetEnv tests the safe lookup of environment variables
+// TestGetEnv tests the safe lookup and retrieval of environment variable values
 func TestGetEnv(t *testing.T) {
-	// Test Empty String
-	val, err := GetEnv("")
-	test.Assert(t, len(val) == 0, "Empty string as argument should return empty string as default")
-	test.Throws(t, err)
+	tests := map[string]struct {
+		export string
+		result string
+	}{
+		"argument error":    {"", ""},
+		"env missing error": {envVarProjectID, ""},
+		"valid env empty":   {envVarProjectID, ""},
+		"valid env":         {envVarProjectID, projectID},
+	}
 
-	// Test ENV missing
-	setup(t)
-	test.Ok(t, os.Unsetenv(envVarProjectID))
-	_, err2 := GetEnv(envVarProjectID)
-	test.Throws(t, err2)
-
-	// VALID ENV empty string should return
-	os.Setenv(envVarProjectID, "")
-	val4, err4 := GetEnv(envVarProjectID)
-	test.Ok(t, err4)
-	test.Assert(t, len(val4) == 0, "Valid ENV set should get value")
-
-	// VALID ENV value should return
-	os.Setenv(envVarProjectID, projectID)
-	val5, err5 := GetEnv(envVarProjectID)
-	test.Assert(t, len(val5) != 0, "Valid ENV set should get value")
-	test.Ok(t, err5)
+	for k, test := range tests {
+		if k == "argument error" {
+			val, err := GetEnv(test.export)
+			testhelper.Assert(t, len(val) == 0, "should return empty string as default")
+			testhelper.Throws(t, err)
+		}
+		if k == "env missing error" {
+			setup(t)
+			testhelper.Ok(t, os.Unsetenv(envVarProjectID))
+			_, err := GetEnv(test.export)
+			testhelper.Throws(t, err)
+		}
+		if k == "valid env empty" {
+			os.Setenv(envVarProjectID, "")
+			val, err := GetEnv(test.export)
+			testhelper.Ok(t, err)
+			testhelper.Assert(t, len(val) == 0, "Valid ENV set should get value")
+		}
+		if k == "valid env" {
+			os.Setenv(envVarProjectID, projectID)
+			val, err := GetEnv(test.export)
+			testhelper.Ok(t, err)
+			testhelper.Assert(t, len(val) != 0, "Valid ENV set should get value")
+		}
+	}
 }
