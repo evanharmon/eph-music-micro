9765e55aaab08db8d5c4cc1bf751aa2f9ce96473
Evan Harmon <evan.p.harmon@gmail.com>
2018-09-17 02:02:23 -0700

    feat(storage): table test for bucket create

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 
 	helper "github.com/evanharmon/eph-music-micro/helper"
-	test "github.com/evanharmon/eph-music-micro/helper/testhelper"
+	testhelper "github.com/evanharmon/eph-music-micro/helper/testhelper"
 )
 
 //
@@ -19,56 +19,60 @@ var (
 // Setup sets the necessary vars from ENV exports
 func setup(t *testing.T) {
 	id, err := helper.GetEnv(envVarProjectID)
-	test.Ok(t, err)
-	test.Assert(t, len(id) != 0, "Export necessary ENV vars before running tests")
+	testhelper.Ok(t, err)
+	testhelper.Assert(t, len(id) != 0, "Export necessary ENV vars before running tests")
 	ProjectID = id
 	bucketName = fmt.Sprintf("%s-test", id)
-}
-
-func beforeConfigureStorage(t *testing.T) {
-	setup(t)
 }
 
 // TestConfigureStoraage tests creating a client for the Google Cloud Storage
 // API
 func TestConfigureStorage(t *testing.T) {
-	beforeConfigureStorage(t)
-	test.Ok(t, ConfigureStorage(bucketName))
-}
-
-func beforeTestCreate(t *testing.T) {
 	setup(t)
-	test.Ok(t, ConfigureStorage(bucketName))
+	testhelper.Ok(t, ConfigureStorage(bucketName))
 }
 
 func TestCreate(t *testing.T) {
-	// test initial bucket create
-	beforeTestCreate(t)
-	ctx := context.Background()
-	test.Ok(t, Create(ctx))
+	setup(t)
+	tests := map[string]struct {
+		name string
+	}{
+		"create success":           {bucketName},
+		"duplicate bucket success": {bucketName},
+		"duplicate bucket failure": {"test"}, // this bucket is always taken in global namespaces
+	}
 
-	// test duplicate bucket creation
-	test.Ok(t, Create(ctx))
+	for k, test := range tests {
+		setup(t)
+		testhelper.Ok(t, ConfigureStorage(test.name))
+		ctx := context.Background()
+		if k == "duplicate bucket failure" {
+			testhelper.Throws(t, Create(ctx))
+		} else {
+			bucketName = fmt.Sprintf("%s-test", ProjectID)
+			testhelper.Ok(t, Create(ctx))
+		}
+	}
 }
 
 func beforeTestDelete(t *testing.T) {
 	setup(t)
-	test.Ok(t, ConfigureStorage(bucketName))
+	testhelper.Ok(t, ConfigureStorage(bucketName))
 	ctx := context.Background()
-	test.Ok(t, Create(ctx))
+	testhelper.Ok(t, Create(ctx))
 }
 
 func TestDelete(t *testing.T) {
 	beforeTestDelete(t)
 	ctx := context.Background()
-	test.Ok(t, Delete(ctx))
+	testhelper.Ok(t, Delete(ctx))
 }
 
 func beforeTestUploadFile(t *testing.T) {
 	setup(t)
-	test.Ok(t, ConfigureStorage(bucketName))
+	testhelper.Ok(t, ConfigureStorage(bucketName))
 	ctx := context.Background()
-	test.Ok(t, Create(ctx))
+	testhelper.Ok(t, Create(ctx))
 }
 
 func TestUploadFile(t *testing.T) {
@@ -78,14 +82,14 @@ func TestUploadFile(t *testing.T) {
 		t.Error(err)
 	}
 	ctx := context.Background()
-	test.Ok(t, UploadFile(ctx, fpath))
+	testhelper.Ok(t, UploadFile(ctx, fpath))
 }
 
 func beforeTestDeleteFile(t *testing.T) {
 	setup(t)
-	test.Ok(t, ConfigureStorage(bucketName))
+	testhelper.Ok(t, ConfigureStorage(bucketName))
 	ctx := context.Background()
-	test.Ok(t, Create(ctx))
+	testhelper.Ok(t, Create(ctx))
 	TestUploadFile(t)
 }
 
@@ -93,5 +97,5 @@ func TestDeleteFile(t *testing.T) {
 	beforeTestDeleteFile(t)
 	fname := "upload-file.txt"
 	ctx := context.Background()
-	test.Ok(t, DeleteFile(ctx, fname))
+	testhelper.Ok(t, DeleteFile(ctx, fname))
 }
